ef42d75f28e1467de90d6945f379f186f59b727f
Juzi <ipif@163.com>
2026-03-04 15:22:52 +0800

    site

--- astro.config.ts
+++ astro.config.ts
@@ -26,7 +26,7 @@ import config from './src/site.config.ts'
 // https://astro.build/config
 export default defineConfig({
   // [Basic]
-  site: 'https://astro-pure.js.org',
+  site: 'https://juzzi.qzz.io',
   // Deploy to a sub path
   // https://astro-pure.js.org/docs/setup/deployment#platform-with-base-path
   // base: '/astro-pure/',
